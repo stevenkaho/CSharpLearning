f4d7d84059f09e589e7940b2301d3a2c3bb8b4e6
stevenkaho <kahocg@gmail.com>
2020-09-24 16:42:18 -0400

    'first-commit'

---
+++ Linq.Query.XML.Demo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq.Query.XML.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> names = from student in XDocument.Load(@"C:\temp\data.xml").Descendants("Student")
+                                         where (int)student.Element("TotalMarks") > 700
+                                         orderby (int)student.Element("TotalMarks") descending
+                                         select student.Element("Name").Value;
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.ReadLine(); 
+                                    
+        }
+    }
+}
--- LinqtoXMLDemo/Program.cs
+++ LinqtoXMLDemo/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
-using System.Linq;
 using System.IO;
 
 
